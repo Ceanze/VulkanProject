a626ed1a68345efe93d8817c94f333c0b4d0d475
Jonathan Åleskog <jonathan9609082000@gmail.com>
2020-03-09 16:40:50 +0100

    Changed farplane distance

--- VulkanProject/src/Sandbox/Tests/ComputeTransferTest.cpp
+++ VulkanProject/src/Sandbox/Tests/ComputeTransferTest.cpp
@@ -25,7 +25,7 @@ void ComputeTransferTest::init()
 	this->graphicsCommandPool.init(CommandPool::Queue::GRAPHICS, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
 	this->transferCommandPool.init(CommandPool::Queue::TRANSFER, 0);
 	this->compCommandPool.init(CommandPool::Queue::COMPUTE, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
-	this->camera = new Camera(getWindow()->getAspectRatio(), 45.f, { 0.f, 20.f, 10.f }, { 0.f, 0.f, 0.f }, 30.0f, 4.0f);
+	this->camera = new Camera(getWindow()->getAspectRatio(), 45.f, { 0.f, 20.f, 10.f }, { 0.f, 0.f, 0.f }, 100.0f, 4.0f);
 	generateHeightmap();
 
 	getShaders().resize(2);
@@ -90,7 +90,7 @@ void ComputeTransferTest::init()
 	setupPost();
 
 	std::string pathToCubemap = "..\\assets\\Textures\\skybox\\";
-	this->skybox.init(700.0f, pathToCubemap, getSwapChain(), &this->graphicsCommandPool, &this->renderPass);
+	this->skybox.init(500.0f, pathToCubemap, getSwapChain(), &this->graphicsCommandPool, &this->renderPass);
 
 	// Record primary indirect draw buffer
 	for (uint32_t i = 0; i < 2; i++)
@@ -421,14 +421,14 @@ void ComputeTransferTest::record(uint32_t id)
 void ComputeTransferTest::generateHeightmap()
 {
 	JAS_PROFILER_FUNCTION();
-	this->heightmap.setVertexDist(0.2f);
-	this->heightmap.setProximitySize(20);
+	this->heightmap.setVertexDist(3.0f);
+	this->heightmap.setProximitySize(30);
 	this->heightmap.setMaxZ(100.f);
 	this->heightmap.setMinZ(0.f);
 
 	int width, height;
 	int channels;
-	std::string path = "../assets/Textures/island.png";
+	std::string path = "../assets/Textures/australia.jpg";
 	unsigned char* data = static_cast<unsigned char*>(stbi_load(path.c_str(), &width, &height, &channels, 1));
 	if (data == nullptr)
 		JAS_ERROR("Failed to load heightmap, couldn't find file!");
@@ -512,7 +512,7 @@ void ComputeTransferTest::transferVertexData()
 			this->activeBuffers ^= 1;
 
 			vkQueueWaitIdle(Instance::get().getGraphicsQueue().queue);
-			//vkQueueWaitIdle(Instance::get().getComputeQueue().queue);
+			vkQueueWaitIdle(Instance::get().getComputeQueue().queue);
 			VkDeviceSize vertexBufferSize = this->verticies.size() * sizeof(Heightmap::Vertex);
 			for (uint32_t i = 0; i < static_cast<uint32_t>(getSwapChain()->getNumImages()); i++)
 			{
